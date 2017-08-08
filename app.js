f1abcc7d2704fe26058bf8c071462d90738f570e
captainfathom <mreynoso@spu.edu>
2017-08-08 16:51:47 -0700

    it works!

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (climb.toLowerCase() === 'yes' || climb.toLowerCase() === 'y') {
 } else if(climb.toLowerCase() === 'no' || climb.toLowerCase === 'n') {
   alert('You are correct! As cool as that would be, the highest peak I\'ve climbed is Navajo Peak at 7223ft.');
 } else {
-  alert('That is not a valid answer. Let\'s just move on...');
+  alert('Please answer yes or no. But, for now, let\'s just move on...');
 }
 console.log('I have climbed every peak over 8000ft in Washington State. Believe me?');
 console.log(climb.toLowerCase());
@@ -21,7 +21,7 @@ if (kids.toLowerCase() === 'yes' || kids.toLowerCase() === 'y') {
 } else if(kids.toLowerCase() === 'no' || kids.toLowerCase() === 'n') {
   alert('I indeed have the two coolest kids on the planet.I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!');
 } else {
-  alert('That is not a valid answer. Let\'s just move on...');
+  alert('Please answer yes or no. But, for now, let\'s just move on...');
 }
 console.log('Do you think I have two boys, 1 and 3 years old, who are the most awesome kids in the world?');
 console.log(kids.toLowerCase());
@@ -33,7 +33,7 @@ if (language.toLowerCase() === 'yes' || language.toLowerCase() === 'y') {
 } else if(language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
   alert('Unfortunately, you are correct. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.');
 } else {
-  alert('That is not a valid answer. Let\'s just move on...');
+  alert('Please answer yes or no. But, for now, let\'s just move on...');
 }
 console.log('Do I speak many languages?');
 console.log(language.toLowerCase());
@@ -45,7 +45,7 @@ if (juggling.toLowerCase() === 'yes' || juggling.toLowerCase() === 'y') {
 } else if(juggling.toLowerCase() === 'no' || juggling.toLowerCase() === 'n') {
   alert('I may look like a world class juggler, but you\'d be wrong');
 } else {
-  alert('That is not a valid answer. Let\'s just move on...');
+  alert('Please answer yes or no. But, for now, let\'s just move on...');
 }
 console.log('I am terrible at juggling. Yes or no?');
 console.log(juggling.toLowerCase());
@@ -57,7 +57,7 @@ if (home.toLowerCase() === 'yes' || home.toLowerCase() === 'y') {
 } else if(home.toLowerCase() === 'no' || home.toLowerCase() === 'n') {
   alert('Well, I think it\'s a kind of island paradise. I\'m from Whidbey Island in the Puget Sound. It is pretty awesome, but not the picture of an island paradise usually imagined');
 } else {
-  alert('That is not a valid answer. Let\'s just move on...');
+  alert('Please answer yes or no. But, for now, let\'s just move on...');
 }
 console.log('I am from a beautiful island paradise. True?');
 console.log(home.toLowerCase());
