82695c59a30d3dd33ad7a1aeaa2b104c529bdd92
captainfathom <mreynoso@spu.edu>
2017-08-08 15:17:19 -0700

    first prompt attempt

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+alert ('Welcome to my page. Before you get to see my page, let\'s see what you already know...or think you know about me.');
+
+var climb = prompt ('I have climbed every peak over 8000ft in Washington State. Believe me?');
+
+if (climb.toLowerCase() = yes || y) {
+  alert('I wish. But, alas, I have climbed to 7223 ft on Navajo Peak, but nothing higher in Washington.')
+} else if(climb.toLowerCase() = no || n) {
+  alert('You are correct! As cool as that would be, the highest peak I\'ve climbed is Navajo Peak at 7223ft.')
+}
