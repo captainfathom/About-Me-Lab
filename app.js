f69b58119a311dde7669b754620beec613a75c0c
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-10 15:12:19 -0700

    Nested Arrays Update

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ alert ('Welcome to my page. Before you get to see my page, let\'s see what you a
 function Score(){
   var score = 0;
   var answerlog = [];
+  var Question6array = [];
   var questionlog = ['I have climbed every peak over 8000ft in Washington State. Believe me?', 'Do you think I have two boys, 1 and 3 years old, who are the most awesome kids in the world?', 'Do I speak many languages?', 'I am terrible at juggling. Yes or no?', 'I am from a beautiful island paradise. True?', 'My dog, Bentley, has been with me through a lot. Can you guess how old he is?', 'Can you guess what states I\'ve lived in other that Washington?'];
+  var reslog = ['I wish. But, alas, I have climbed only to 7223 ft on Navajo Peak, but nothing higher in Washington.', 'You are correct! As cool as that would be, the highest peak I\'ve climbed is Navajo Peak at 7223ft.', 'Please answer yes or no. But, for now, let\'s just move on...', 'Yes! I have the two most awesome boys in the world. I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!', 'I indeed have the two coolest kids on the planet. I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!', 'Please answer yes or no. But, for now, let\'s just move on...', 'Nope. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.', 'Nope. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.', 'Please answer yes or no. But, for now, let\'s just move on...', 'I\'m mildly offended that you assume I can\'t juggle, jk, but you are correct.', 'I may look like a world class juggler, but you would be wrong.', 'Please answer yes or no. But, for now, let\'s just move on...', 'You\'re right! (mostly) I\'m from Whidbey Island in the Puget Sound. It is pretty awesome, but not the picture of an island paradise usually imagined.', 'Well, I think it\'s a kind of island paradise. I\'m from Whidbey Island in the Puget Sound. It is pretty awesome, but not the picture of an island paradise usually imagined', 'Please answer yes or no. But, for now, let\'s just move on...', 'Nope. Hint, he\'s a wise old soul.', 'I hope he makes it that long, but he\'s not there yet.', 'That\'s right! He\'s 11 years old and going strong!', 'Thanks for playing. Bentley is 11 years old and going strong!', 'You must answer with a number', ['You got one! I\'ve lived in Washington, ', '.'], ['Nope. Try again. You have ', ' left.'], ['Oh No! Better luck next time. I lived in Washington, ', '.']];
+  console.log(reslog[20][0]);
   function Global(){
     function Question1(){
       var firstQuestion = questionlog[0];
       var climb = prompt (firstQuestion);
       answerlog.push(climb);
       if (climb.toLowerCase() === 'yes' || climb.toLowerCase() === 'y') {
-        alert('I wish. But, alas, I have climbed only to 7223 ft on Navajo Peak, but nothing higher in Washington.');
-      } else if(climb.toLowerCase() === 'no' || climb.toLowerCase === 'n') {
-        alert('You are correct! As cool as that would be, the highest peak I\'ve climbed is Navajo Peak at 7223ft.');
+        alert(reslog[0]);
+      } else if(climb.toLowerCase() === 'no' || climb.toLowerCase() === 'n') {
+        alert(reslog[1]);
         score++;
       } else {
-        alert('Please answer yes or no. But, for now, let\'s just move on...');
+        alert(reslog[2]);
       }
       console.log(firstQuestion);
       console.log(climb.toLowerCase());
@@ -30,12 +33,12 @@ function Score(){
       var kids = prompt (secondQuestion);
       answerlog.push(kids);
       if (kids.toLowerCase() === 'yes' || kids.toLowerCase() === 'y') {
-        alert('Yes! I have the two most awesome boys in the world. I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!');
+        alert(reslog[3]);
         score++;
       }else if(kids.toLowerCase() === 'no' || kids.toLowerCase() === 'n') {
-        alert('I indeed have the two coolest kids on the planet.I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!');
+        alert(reslog[4]);
       }else {
-        alert('Please answer yes or no. But, for now, let\'s just move on...');
+        alert(reslog[5]);
       }
       console.log(secondQuestion);
       console.log(kids.toLowerCase());
@@ -47,12 +50,12 @@ function Score(){
       var language = prompt (thirdQuestion);
       answerlog.push(language);
       if (language.toLowerCase() === 'yes' || language.toLowerCase() === 'y') {
-        alert('Nope. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.');
+        alert(reslog[6]);
       } else if(language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
-        alert('Unfortunately, you are correct. Although I am half Mexican and my dad\'s first language is Spanish, I  only speak English.');
+        alert(reslog[7]);
         score++;
       } else {
-        alert('Please answer yes or no. But, for now, let\'s just move on...');
+        alert(reslog[8]);
       }
       console.log(thirdQuestion);
       console.log(language.toLowerCase());
@@ -67,12 +70,12 @@ function Score(){
       var juggling = prompt (fourthQuestion);
       answerlog.push(juggling);
       if (juggling.toLowerCase() === 'yes' || juggling.toLowerCase() === 'y') {
-        alert('I\'m mildly offended that you assume I can\'t juggle, jk, but you are correct.');
+        alert(reslog[9]);
         score++;
       } else if(juggling.toLowerCase() === 'no' || juggling.toLowerCase() === 'n') {
-        alert('I may look like a world class juggler, but you\'d be wrong');
+        alert(reslog[10]);
       } else {
-        alert('Please answer yes or no. But, for now, let\'s just move on...');
+        alert(reslog[11]);
       }
       console.log(fourthQuestion);
       console.log(juggling.toLowerCase());
@@ -83,12 +86,12 @@ function Score(){
       var home = prompt (fifthQuestion);
       answerlog.push(home);
       if (home.toLowerCase() === 'yes' || home.toLowerCase() === 'y') {
-        alert('You\'re right! (mostly) I\'m from Whidbey Island in the Puget Sound. It is pretty awesome, but not the picture of an island paradise usually imagined.');
+        alert(reslog[12]);
         score++;
       } else if(home.toLowerCase() === 'no' || home.toLowerCase() === 'n') {
-        alert('Well, I think it\'s a kind of island paradise. I\'m from Whidbey Island in the Puget Sound. It is pretty awesome, but not the picture of an island paradise usually imagined');
+        alert(reslog[13]);
       } else {
-        alert('Please answer yes or no. But, for now, let\'s just move on...');
+        alert(reslog[14]);
       }
       console.log(fifthQuestion);
       console.log(home.toLowerCase());
@@ -107,19 +110,19 @@ function Score(){
         console.log(parseInt(oldDog));
         if (parseInt(oldDog) < 11) {
           attempts--;
-          alert ('Nope. Hint, he\'s a wise old soul.');
+          alert (reslog[15]);
         } else if (parseInt(oldDog) > 11) {
           attempts--;
-          alert ('I hope he makes it that long, but he\'s not there yet.');
+          alert (reslog[16]);
         } else if (parseInt(oldDog) === 11) {
-          alert ('That\'s right! He\'s 11 years old and going strong!');
+          alert (reslog[17]);
           answerlog.push(oldDog);
           score++;
           console.log(score);
           break;
         } else if (attempts === 1){
-          alert ('Thanks for playing. Bentley is 11 years old and going strong!');
-        } else {alert('You must answer with a number');
+          alert (reslog[18]);
+        } else {alert(reslog[19]);
         }
       }
     }
@@ -135,15 +138,15 @@ function Score(){
         console.log(seventhQuestion);
         console.log(state);
         if (livedState.includes(state.toLowerCase())) {
-          alert ('You got one! I\'ve lived in Washington, ' + printLivedState + '.');
+          alert (reslog[20].join(printLivedState));
           answerlog.push(state);
           score++;
           break;
         } else if (guesses > 1){
           guesses--;
-          alert ('Nope. Try again. You have ' + guesses + ' left.');
+          alert (reslog[21].join(guesses));
         } else if (guesses === 1){
-          alert('Oh No! Better luck next time. I lived in ' + printLivedState + '.');
+          alert(reslog[22].join(printLivedState));
           guesses--;
         }
       }
