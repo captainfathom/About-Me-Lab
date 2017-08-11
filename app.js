36e7fc09656a9ec9deea60d6dcc206bf8f3f1a86
Mark Reynoso <mreynoso@spu.edu>
2017-08-10 17:39:00 -0700

    pull pair coding lab

--- app.js
+++ app.js
@@ -2,49 +2,6 @@
 
 alert ('Welcome to my page. Before you get to see my page, let\'s see what you already know...or think you know about me. In this little game, there\'s 7 superpower points available. Good luck.');
 
-<<<<<<< HEAD
-var score = 0;
-
-var climb = prompt ('I have climbed every peak over 8000ft in Washington State. Believe me?');
-
-if (climb.toLowerCase() === 'yes' || climb.toLowerCase() === 'y') {
-  alert('I wish. But, alas, I have climbed only to 7223 ft on Navajo Peak, but nothing higher in Washington.');
-} else if(climb.toLowerCase() === 'no' || climb.toLowerCase() === 'n') {
-  alert('You are correct! As cool as that would be, the highest peak I\'ve climbed is Navajo Peak at 7223ft.');
-  score++;
-} else {
-  alert('Please answer yes or no. But, for now, let\'s just move on...');
-}
-console.log('I have climbed every peak over 8000ft in Washington State. Believe me?');
-console.log(climb.toLowerCase());
-
-var kids = prompt ('Do you think I have two boys, 1 and 3 years old, who are the most awesome kids in the world?');
-
-if (kids.toLowerCase() === 'yes' || kids.toLowerCase() === 'y') {
-  alert('Yes! I have the two most awesome boys in the world. I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!');
-  score++;
-} else if(kids.toLowerCase() === 'no' || kids.toLowerCase() === 'n') {
-  alert('I indeed have the two coolest kids on the planet.I say this without the ability to know whether this is an objective or subjective truth, but it is definitly true to me!!!');
-} else {
-  alert('Please answer yes or no. But, for now, let\'s just move on...');
-}
-console.log('Do you think I have two boys, 1 and 3 years old, who are the most awesome kids in the world?');
-console.log(kids.toLowerCase());
-
-var language = prompt ('Do I speak many languages?');
-
-if (language.toLowerCase() === 'yes' || language.toLowerCase() === 'y') {
-  alert('Nope. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.');
-} else if(language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
-  alert('Unfortunately, you are correct. Although I am half Mexican and my dad\'s first language is Spanish, I only speak English.');
-  score++;
-} else {
-  alert('Please answer yes or no. But, for now, let\'s just move on...');
-}
-console.log('Do I speak many languages?');
-console.log(language.toLowerCase());
-
-=======
 function Score(){
   var score = 0;
   var answerlog = [];
@@ -104,9 +61,8 @@ function Score(){
       console.log(language.toLowerCase());
     }
     Question3();
->>>>>>> 3936485f1dd40a478a7cc78eec6fc1e24721789c
-//score update
 
+//score update
     alert ('You\'ve got ' + score + '/7 so far. Let\'s keep going!');
 
     function Question4(){
